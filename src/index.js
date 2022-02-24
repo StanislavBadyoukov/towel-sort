167a2e8cb2d4144d644513a9464ba640556b7ce1
StanislavBadyoukov <stanislav.badyoukov@mail.ru>
2022-02-24 23:37:57 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let array = [];
+  if (matrix === undefined) {return array}
+  for (let i = 0; i < matrix.length; i++){
+  if (i%2 === 1) {
+    array = array.concat(matrix[i].reverse())
+  }
+  else {
+    array = array.concat(matrix[i])
+  }	
+}
+return array;
 }
